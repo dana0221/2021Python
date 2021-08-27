1bd8f4f6a95161a521535520160cfd326f32b373
dana8 <w2008@e-mirim.hs.kr>
2021-08-27 16:07:01 +0900

    package 1/2 commit

---
+++ module, package/tour/gangwondo.py
@@ -0,0 +1,11 @@
+class GangwondoPackage:
+    def __init__(self):
+        self.places = ['강릉 경포대 해수욕장', '속초 바다정원 카페']
+
+    def __str__(self):
+        return str(self.places)
+
+
+if __name__ == '__main__':
+    gp = GangwondoPackage()
+    print(gp)
