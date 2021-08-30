d9fe154c4f6e7afc8c4d0bf392b025171e2c378f
dana8 <w2008@e-mirim.hs.kr>
2021-08-30 12:08:01 +0900

    package 2/2 commit

--- module, package/do.py
+++ module, package/do.py
@@ -1,4 +1,25 @@
-import my_modul
-# my_modul에 있는 모든 내용을 가져옴
+# # import my_modul
+# # my_modul에 있는 모든 내용을 가져옴
+#
+# import tour.jeonnam
+# tour.jeonnam.채리()
+#
+# from tour import jeonnam
+# jeonnam.채리()
+#
+# from tour.jeonnam import 채리
+# 채리()
 
-my_modul.나라()
+# import tour.jeonnam as tj
+# tj.채리()
+#
+# from tour import jeonnam as je
+# je.채리()
+#
+# from tour.jeonnam import 채리 as cherry
+# cherry()
+
+from tour import *
+jeonnam.채리()
+gp = gangwondo.GangreungPackage()
+print(gp)
