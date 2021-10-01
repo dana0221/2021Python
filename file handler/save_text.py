21b9f47db1266795e1da829b256af504fd94ef89
dana8 <w2008@e-mirim.hs.kr>
2021-10-01 15:37:11 +0900

    Edit save_text.py with Commit

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -7,3 +7,11 @@ f.write('양가영')
 f.write('검정색')
 
 f.close()
+
+# 위의 코드와 동일하지만 close()를 따로 작성하지 않아도 자동으로 닫아줌
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.write('양가영')
+    f.write('보라색')
+    f.write('\n')
+    f.write('양가영')
+    f.write('검정색')
